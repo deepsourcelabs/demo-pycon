1611c2c7d630d648c04044ccaf7c4cb0a703ab85
Srijan Saurav <68371686+srijan-deepsource@users.noreply.github.com>
2020-10-02 17:56:19 +0530

    Update consider_in.py

--- consider_in.py
+++ consider_in.py
@@ -4,13 +4,13 @@ def check(x):
     elif x != 2 or x != 3:
         print("also true")
 
-    elif x in (2, 3) or x in (5, 4):
+    elif x in (2, 3, 4) or x in (5, 4):
         print("Here")
 
     elif (
         x == 10
         or x == 20
-        or x == 30
+        or x == 40
         and x == 40
     ):
         print("Why even?")
@@ -21,3 +21,5 @@ def check(x):
         or x == 30
     ):
         print("Why even?")
+
+
